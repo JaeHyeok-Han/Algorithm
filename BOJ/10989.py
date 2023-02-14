8713144c6e9af4f1fb050e4b00e4319a50ceb02d
JaeHyeok-Han <hjh0633@gmail.com>
2023-02-14 19:57:35 +0900

    BOJ 10989 메모리 초과 수정

--- BOJ/10989.py
+++ BOJ/10989.py
@@ -1,9 +1,10 @@
 import sys
-from heapq import heappush
 
 number = int(sys.stdin.readline())
-numbers = []
-for number in range(number):
-    heappush(numbers, int(sys.stdin.readline()))
-for number in numbers:
-    print(number)
+numbers = [0] * 10001
+for _ in range(number):
+    numbers[int(sys.stdin.readline())] += 1
+for i in range(10001):
+    if numbers[i] != 0:
+        for _ in range(numbers[i]):
+            print(i)
